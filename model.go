701ceaf81f0c24dcc586f12aa0a956d504a94e40
Julia Samodurova <157882941+juliasamodurova@users.noreply.github.com>
2025-02-17 00:29:32 +0300

    Delete model.go

--- model.go
+++
@@ -1,9 +0,0 @@
-package app
-
-// Product - структура данных для товара
-type Product struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Price       int    `json:"price"`
-}
